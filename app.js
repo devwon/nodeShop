f5a9177858c07accce92e1156db0fdd63ec8747a
1hyerin <wonhyerin96@gmail.com>
2018-01-17 22:21:39 +0900

    nodemon으로 바꾸기

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ var port = 3000;
 
 
 app.get('/', function (req, res) {//get방식으로 보내기
-    res.send('first app');
+    res.send('first app12');
 });
 
+app.get('/admin',function(req,res){//admin 페이지
+    res.send('admin page');
+});
 app.listen(port, function () { //서버에 띄어주는
     console.log('Express listening on port', port);
 });
