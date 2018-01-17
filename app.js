b5c6fd8af120a2478c04589014d8b79b3239b4e0
1hyerin <wonhyerin96@gmail.com>
2018-01-17 22:05:36 +0900

    express모듈 사용

---
+++ app.js
@@ -0,0 +1,15 @@
+//express module 설치했으니 require('express')사용가능
+
+var express = require('express');
+
+var app = express();
+var port = 3000;
+
+
+app.get('/', function (req, res) {//get방식으로 보내기
+    res.send('first app');
+});
+
+app.listen(port, function () { //서버에 띄어주는
+    console.log('Express listening on port', port);
+});
